d87759559b02de2d424027a3f7630ef1ee2fa603
warrior14 <ironaddicts14@gmail.com>
2021-05-27 18:54:54 -0500

    Made an array of grocery objects

---
+++ shopping.js
@@ -0,0 +1,39 @@
+const groceryList = [
+    {
+        id: 1,
+        name: "Cheese",
+        price: "5 dollars"        
+    },
+    {
+        id: 2,
+        name: "Chicken Thighs",
+        price: "10 dollars"
+    },
+    {
+        id: 3,
+        name: "Tomatoes",
+        price: "2 dollars"
+    },
+    {
+        id: 4,
+        name: "Pork Chops",
+        price: "7 dollars"
+    },
+    {
+        id: 5,
+        name: "Sushi",
+        price: "8 dollars"
+    },
+    {
+        id: 6,
+        name: "Wild Rice",
+        price: "4 dollars"
+    },
+    {
+        id: 7,
+        name: "Angus Steak",
+        price: "15 dollars"
+    }
+];
+
+console.log(groceryList);
